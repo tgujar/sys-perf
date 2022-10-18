8969a1b5f1852362437400e41dda2fa2800e7133
Manoj Kilaru <manoj@Rohiths-MacBook-Pro.local>
2022-10-17 22:32:26 -0700

    Adedd CSV writter

---
+++ utils/testCsv.cpp
@@ -0,0 +1,23 @@
+#include "csvfile.h"
+
+int main()
+{
+    try
+    {
+        csvfile out("../data/ctxswitchout.csv");
+        out << "X" << "VALUE" << endrow;
+        int i = 1;
+        out << i++ << "String value" << endrow;
+        out << i++ << 123 << endrow;
+        out << i++ << 1.f << endrow;
+        out << i++ << 1.2 << endrow;
+        out << i++ << "One more string" << endrow;
+        out << i++ << "\"Escaped\"" << endrow;
+        out << i++ << "=HYPERLINK(\"https://playkey.net\"; \"Playkey Service\")" << endrow;
+    }
+    catch (const std::exception &ex)
+    {
+        std::cout << "Exception was thrown: " << ex.what() << std::endl;
+    }
+    return 0;
+}
