8c580829daaac8b20e05ee3c4318899de8780080
Tanmay Gujar <tgujar@ucsd.edu>
2022-12-06 08:41:27 +0000

    added exp and data

--- src/Filesystem/remote_createFile.sh
+++ src/Filesystem/remote_createFile.sh
@@ -6,6 +6,3 @@ dd if=/dev/urandom of=Filesystem/temp/file64M.txt bs=1M count=64
 dd if=/dev/urandom of=Filesystem/temp/file128M.txt bs=1M count=128
 dd if=/dev/urandom of=Filesystem/temp/file256M.txt bs=1M count=256
 dd if=/dev/urandom of=Filesystem/temp/file512M.txt bs=1M count=512
-dd if=/dev/urandom of=Filesystem/temp/file1G.txt bs=1G count=1
-dd if=/dev/urandom of=Filesystem/temp/file2G.txt bs=1G count=2
-dd if=/dev/urandom of=Filesystem/temp/file4G.txt bs=1G count=4
--- src/Filesystem/remote_readtime_sequential.cpp
+++ src/Filesystem/remote_readtime_sequential.cpp
@@ -12,13 +12,13 @@
 #include "stats.hpp"
 #include "proc.hpp"
 
-const char*  filenames[12] = {"Filesystem/temp/file4M.txt", "Filesystem/temp/file8M.txt", "Filesystem/temp/file16M.txt", "Filesystem/temp/file32M.txt", "Filesystem/temp/file64M.txt", "Filesystem/temp/file128M.txt", "Filesystem/temp/file256M.txt", "Filesystem/temp/file512M.txt", "Filesystem/temp/file1G.txt", "Filesystem/temp/file2G.txt", "Filesystem/temp/file4G.txt", "Filesystem/temp/file8G.txt"};
-int filesizes[12] = {4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
+const char*  filenames[8] = {"Filesystem/temp/file4M.txt", "Filesystem/temp/file8M.txt", "Filesystem/temp/file16M.txt", "Filesystem/temp/file32M.txt", "Filesystem/temp/file64M.txt", "Filesystem/temp/file128M.txt", "Filesystem/temp/file256M.txt", "Filesystem/temp/file512M.txt"};
+int filesizes[8] = {4, 8, 16, 32, 64, 128, 256, 512};
 int fileNum = 0;
 
 using namespace std;
 
-double rand_read_time_chrono_innerloop() {
+double seq_read_time_chrono_innerloop() {
     int blocksize = 4*1024;
     int numOfBlocksinMB = (1024*1024)/(4*1024);
 
@@ -40,20 +40,15 @@ double rand_read_time_chrono_innerloop() {
     }
 
     void* bf = malloc(blocksize);
-    const int iter = 10000;
-    long array[iter];
-    const int numOfBlocks = sizeinMB * numOfBlocksinMB;
+    const int iter = 10;
+    const int numOfBlocks = sizeinMB * numOfBlocksinMB - iter - 1;
     time_t t;
     srand((unsigned) time(&t));
-    for(int i=0; i < iter; i++)
-    {
-        array[i] = rand() %numOfBlocks ;
-        array[i] = array[i]*blocksize;
-    }
-    off_t offset;
+    long offset = rand() % numOfBlocks;
+    offset = offset*blocksize;
+    lseek(fd, offset, SEEK_SET);
     auto start = chrono::steady_clock::now();
     for(ssize_t i = iter; i > 0; i--) {
-        offset = lseek(fd, array[i], SEEK_SET);
         read(fd, bf, blocksize);
     }
     auto end = chrono::steady_clock::now();
@@ -62,7 +57,7 @@ double rand_read_time_chrono_innerloop() {
     return double(chrono::duration_cast<chrono::microseconds>(end - start).count()) / iter;
 }
 
-double rand_read_time_rtdsc_innerloop() {
+double seq_read_time_rtdsc_innerloop() {
     int blocksize = 4*1024;
     int numOfBlocksinMB = (1024*1024)/(4*1024);
 
@@ -76,21 +71,16 @@ double rand_read_time_rtdsc_innerloop() {
     }
 
     void* bf = malloc(blocksize);
-    const int iter = 10000;
-    long array[iter];
-    const int numOfBlocks = sizeinMB * numOfBlocksinMB;
+    const int iter = 10;
+    const int numOfBlocks = sizeinMB * numOfBlocksinMB - iter - 1;
     time_t t1;
     srand((unsigned) time(&t1));
-    for(int i=0; i < iter; i++)
-    {
-        array[i] = rand() %numOfBlocks ;
-        array[i] = array[i]*blocksize;
-    }
-    off_t offset;
+    long offset = rand() % numOfBlocks;
+    offset = offset*blocksize;
+    lseek(fd, offset, SEEK_SET);
     Timer t;
     t.begin();
     for(ssize_t i = iter; i > 0; i--) {
-        offset = lseek(fd, array[i], SEEK_SET);
         read(fd, bf, blocksize);
     }
     t.end();
@@ -100,15 +90,15 @@ double rand_read_time_rtdsc_innerloop() {
 }
 
 int main() {
-    for (int i = 0; i < 11; i++) {
+    for (int i = 0; i < 8; i++) {
         fileNum = i;
         cout<<"Size of the file:"<<filesizes[i]<<endl;
-        Stats<double> s(100), t(100);
+        Stats<double> s(10), t(10);
         use_cores(vector<int> {0});
-        s.run_func(rand_read_time_chrono_innerloop);
+        s.run_func(seq_read_time_chrono_innerloop);
         cout << "Mean (chrono): "<< s.mean() << " us"<< endl;
         cout << "Standard deviation (chrono): "<< s.std_dev() << " us"<< endl;
-        t.run_func(rand_read_time_rtdsc_innerloop);
+        t.run_func(seq_read_time_rtdsc_innerloop);
         cout << "Mean (rtdsc): "<< t.mean() << " us" << endl;
         cout << "Standard deviation (rtdsc): "<< t.std_dev() << " us"<< endl;
         cout<<endl<<endl;
